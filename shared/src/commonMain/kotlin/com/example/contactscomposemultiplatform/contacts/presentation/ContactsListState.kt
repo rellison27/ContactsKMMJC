ff0ae3cbfa55a93a55d9ed674cfc9235aeea2226
Rashaun Ellison <rellison27@gmail.com>
2023-11-20 08:20:31 -0500

    adding state for contact list

---
+++ shared/src/commonMain/kotlin/com/example/contactscomposemultiplatform/contacts/presentation/ContactsListState.kt
@@ -0,0 +1,15 @@
+package com.example.contactscomposemultiplatform.contacts.presentation
+
+import com.example.contactscomposemultiplatform.contacts.domain.Contact
+
+data class ContactsListState(
+    val contacts: List<Contact> = emptyList(),
+    val recentlyAddedContacts: List<Contact> = emptyList(),
+    val selectedContact: Contact? = null,
+    val isAddContactSheetOpen: Boolean = false,
+    val isSelectedContactSheetOpen: Boolean = false,
+    val firstNameError: String? = null,
+    val lastNameError: String? = null,
+    val emailError: String? = null,
+    val phoneNumberError: String? = null
+)
