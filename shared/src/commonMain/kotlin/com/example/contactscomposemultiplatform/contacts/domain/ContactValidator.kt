69450363c9a480d4009e4da918a4c166af92f37b
Rashaun Ellison <rellison27@gmail.com>
2024-07-12 08:36:49 -0400

    Adding validator object

---
+++ shared/src/commonMain/kotlin/com/example/contactscomposemultiplatform/contacts/domain/ContactValidator.kt
@@ -0,0 +1,33 @@
+package com.example.contactscomposemultiplatform.contacts.domain
+
+object ContactValidator
+{
+    data class ValidationResult(
+        val firstNameError: String? = null,
+        val lastNameError: String? = null,
+        val emailError: String? = null,
+        val phoneNumberError: String? = null
+    )
+    fun validateContact(contact: Contact): ValidationResult {
+        var result = ValidationResult()
+
+        if(contact.firstName.isBlank()) {
+            result = result.copy(firstNameError = "Fill out the First name is field")
+        }
+        if(contact.lastName.isBlank()) {
+            result = result.copy(lastNameError = "Fill out the Last name is field")
+        }
+
+        val emailRegex = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+        if(!emailRegex.matches(contact.email)) {
+            result = result.copy(emailError = "The email is not valid")
+        }
+        if(contact.phoneNumber.isBlank()) {
+            result = result.copy(firstNameError = "Fill out the First name is field")
+        }
+
+        return result
+    }
+
+
+}
